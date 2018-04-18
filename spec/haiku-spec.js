b92c2f2d907a7d02e5c44172267202b3c714451a
Kayl Eubanks and Megan Lee <megan@>
2018-04-18 16:14:05 -0700

    wrote out a third test. Does not pass

--- spec/haiku-spec.js
+++ spec/haiku-spec.js
@@ -2,14 +2,19 @@ import {Haiku} from './../src/main.js';
 
 describe('Haiku', function() {
 
-  // it('should convert all three lines of submitted haiku into one array', function() {
-  //   let haikuInput = new Haiku("this is line 1", "this is line 2", "this is line 3");
-  //   expect(haikuInput.arrayConversion()).toEqual([["this", "is", "line", "1"],["this", "is", "line", "2"],["this", "is", "line", "3"]]);
-  // });
+  it('should convert all three lines of submitted haiku into one array', function() {
+    let haikuInput = new Haiku("this is line 1", "this is line 2", "this is line 3");
+    expect(haikuInput.arrayConversion()).toEqual([["this", "is", "line", "1"],["this", "is", "line", "2"],["this", "is", "line", "3"]]);
+  });
 
   it('should check that all characters used are letters', function () {
     let haikuInput = new Haiku("this is line 1", "this is line 2", "this is line 3");
-    // haikuInput.arrayConversion();
     expect(haikuInput.characterChecker(haikuInput)).toEqual(false);
+  });
+
+  it('should verify that lines one and three have five syllables each', function() {
+    let haikuInput = new Haiku("I don't even know where that came from", "this one is correct you know", "this is also a failure");
+    haikuInput.arrayConversion();
+    expect(haikuInput.syllableChecker(haikuInput)).toEqual(false);
   });
 });
--- src/main.js
+++ src/main.js
@@ -6,15 +6,15 @@ export class Haiku{
     this.line3 = line3;
   }
 
-  // arrayConversion() {
-  //   let line1Array = this.line1.split(" ");
-  //   let line2Array = this.line2.split(" ");
-  //   let line3Array = this.line3.split(" ");
-  //   let haikuArray = [];
-  //   haikuArray.push(line1Array, line2Array, line3Array);
-  //   this.array = haikuArray;
-  //   return haikuArray;
-  // }
+  arrayConversion() {
+    let line1Array = this.line1.split(" ");
+    let line2Array = this.line2.split(" ");
+    let line3Array = this.line3.split(" ");
+    let haikuArray = [];
+    haikuArray.push(line1Array, line2Array, line3Array);
+    this.array = haikuArray;
+    return haikuArray;
+  }
 
   characterChecker(object) {
     const arrayToCheck = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
@@ -31,10 +31,8 @@ export class Haiku{
       }
     });
     return x;
-      //if (this.line1.match(/[\W1234567890]+/g) === true || this.line2.match(/[\W1234567890]+/g) === true || this.line3.match(/[\W1234567890]+/g) === true) {
-    //   return false;
-    // } else {
-    //   console.log("FUCKING WORK DAMNIT");
-    // }
   }
+
+  syllableChecker()
+
 }
