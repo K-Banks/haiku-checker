9ec20cfcc7c696d3dcdcbf0ebdfec5d195fe746b
Kayl Eubanks and Megan Lee <megan@>
2018-04-18 15:06:04 -0700

    created and passed second test

--- spec/haiku-spec.js
+++ spec/haiku-spec.js
@@ -2,14 +2,14 @@ import {Haiku} from './../src/main.js';
 
 describe('Haiku', function() {
 
-  it('should convert all three lines of submitted haiku into one array', function() {
-    let haikuInput = new Haiku("this is line 1", "this is line 2", "this is line 3");
-    expect(haikuInput.arrayConversion()).toEqual([["this", "is", "line", "1"],["this", "is", "line", "2"],["this", "is", "line", "3"]]);
-  });
+  // it('should convert all three lines of submitted haiku into one array', function() {
+  //   let haikuInput = new Haiku("this is line 1", "this is line 2", "this is line 3");
+  //   expect(haikuInput.arrayConversion()).toEqual([["this", "is", "line", "1"],["this", "is", "line", "2"],["this", "is", "line", "3"]]);
+  // });
 
   it('should check that all characters used are letters', function () {
     let haikuInput = new Haiku("this is line 1", "this is line 2", "this is line 3");
-    let haikuArray = haikuInput.arrayConversion();
-    expect(characterChecker(haikuArray)).toEqual(true);
+    // haikuInput.arrayConversion();
+    expect(haikuInput.characterChecker(haikuInput)).toEqual(false);
   });
 });
--- src/main.js
+++ src/main.js
@@ -6,18 +6,35 @@ export class Haiku{
     this.line3 = line3;
   }
 
-  arrayConversion() {
-    let line1Array = this.line1.split(" ");
-    let line2Array = this.line2.split(" ");
-    let line3Array = this.line3.split(" ");
-    let haikuArray = [];
-    haikuArray.push(line1Array, line2Array, line3Array);
-    return haikuArray;
-  }
+  // arrayConversion() {
+  //   let line1Array = this.line1.split(" ");
+  //   let line2Array = this.line2.split(" ");
+  //   let line3Array = this.line3.split(" ");
+  //   let haikuArray = [];
+  //   haikuArray.push(line1Array, line2Array, line3Array);
+  //   this.array = haikuArray;
+  //   return haikuArray;
+  // }
 
-  characterChecker(array) {
-    if(array.includes(/`1234567809-=~!@#\$%\^&*()_+`/g)=== true) {
-      return false;
-    }
+  characterChecker(object) {
+    const arrayToCheck = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    let x = true;
+    arrayToCheck.forEach(function(character) {
+      if (object.line1.includes(character) === true) {
+        x = false;
+      }
+      if (object.line2.includes(character) === true) {
+        x = false;
+      }
+      if (object.line3.includes(character) === true) {
+        x = false;
+      }
+    });
+    return x;
+      //if (this.line1.match(/[\W1234567890]+/g) === true || this.line2.match(/[\W1234567890]+/g) === true || this.line3.match(/[\W1234567890]+/g) === true) {
+    //   return false;
+    // } else {
+    //   console.log("FUCKING WORK DAMNIT");
+    // }
   }
 }
