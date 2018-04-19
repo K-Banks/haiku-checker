4deaf5bba0c29caebddce1cfdd9e8618f39222ad
Kayl Eubanks and Megan Lee <kayl@>
2018-04-19 16:25:58 -0700

    Added exceptions to syllable test

--- src/checker.js
+++ src/checker.js
@@ -1,17 +1,15 @@
 class Checker {
-
-
-  characterChecker(object) {
+  characterChecker(haiku) {
     const arrayToCheck = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
     let x = true;
     arrayToCheck.forEach(function(character) {
-      if (object.line1.includes(character) === true) {
+      if (haiku.line1.includes(character) === true) {
         x = false;
       }
-      if (object.line2.includes(character) === true) {
+      if (haiku.line2.includes(character) === true) {
         x = false;
       }
-      if (object.line3.includes(character) === true) {
+      if (haiku.line3.includes(character) === true) {
         x = false;
       }
     });
@@ -19,48 +17,42 @@ class Checker {
   }
 
   syllableChecker(haiku) {
-    let syllables = [];
     const regEx  = new RegExp('[aeiouy]');
     const vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
-    haiku.array[0].forEach(function(word){
-      let length = word.length-1;
-      if (word[length] === 'e') {
-        word = word.replace(word[length], "x");
-        console.log(word);
-      }
-      if (word.length === 1) {
-        syllables.push(word);
-      } else {
-        for (var i = 0; i < word.length; i++) {
-          vowels.forEach(function(vowel) {
-            if (word[i] === vowel && word[i+1] !== undefined) {
-              console.log(word[i]);
-              console.log(word[i+1]);
-              let testLetter = word[i+1];
-              console.log(testLetter);
-              let tester = testLetter.match(regEx);
-              if (tester === null) {
+    for (var b = 0; b < 3; b++) {
+      let syllables = [];
+      haiku.array[b].forEach(function(word){
+        let length = word.length-1;
+        if (word[length] === 'e') {
+          word = word.replace(word[length], "x");
+        }
+        if (word.length === 1) {
+          syllables.push(word);
+        } else {
+          for (var i = 0; i < word.length; i++) {
+            vowels.forEach(function(vowel) {
+              if (word[i] === vowel && word[i+1] !== undefined) {
+                let testLetter = word[i+1];
+                let tester = testLetter.match(regEx);
+                if (tester === null) {
+                  let syllable = word.slice(i);
+                  syllables.push(syllable);
+                }
+              } else if (word[i] === vowel) {
                 let syllable = word.slice(i);
                 syllables.push(syllable);
               }
-            } else if (word[i] === vowel) {
-              let syllable = word.slice(i);
-              syllables.push(syllable);
-            }
-          });
+            });
+          }
         }
-        console.log(syllables);
+      });
+      if (syllables.length !== 5 && b === 0 || b === 2 && syllables.length !== 5 || b === 1 && syllables.length !== 7) {
+        return false;
+      } else {
+        return true;
       }
-    });
-    if (syllables.length !== 5) {
-      return false;
     }
   }
 }
 
 export {Checker};
-
-// && ("[aeiouy]([^aieouy]|$)", 'gim')
-// if (word[length] === 'e') {
-//   syllables.push(0);
-// } else
